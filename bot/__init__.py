88b5c29fddc48dfe98751f82d1799383f8e8e7e6
Balapriyan B <131746104+BalaPriyan@users.noreply.github.com>
2024-04-11 01:10:09 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -147,10 +147,23 @@ if DATABASE_URL:
 else:
     config_dict = {}
 
-if not ospath.exists(".netrc"):
-    with open(".netrc", "w"):
+if not ospath.exists('.netrc'):
+    with open('.netrc', 'w'):
         pass
-run("chmod 600 .netrc && cp .netrc /root/.netrc && chmod +x moro.sh && ./moro.sh",shell=True,)
+srun(["chmod", "600", ".netrc"])
+srun(["cp", ".netrc", "/root/.netrc"])
+trackers = check_output("curl -Ns https://raw.githubusercontent.com/XIU2/TrackersListCollection/master/all.txt https://ngosang.github.io/trackerslist/trackers_all_http.txt https://newtrackon.com/api/all https://raw.githubusercontent.com/hezhijie0327/Trackerslist/main/trackerslist_tracker.txt | awk '$0' | tr '\n\n' ','", shell=True).decode('utf-8').rstrip(',')
+with open("a2c.conf", "a+") as a:
+    if TORRENT_TIMEOUT is not None:
+        a.write(f"bt-stop-timeout={TORRENT_TIMEOUT}\n")
+    a.write(f"bt-tracker=[{trackers}]")
+srun([moko, "--conf-path=/usr/src/app/a2c.conf"])
+alive = Popen(["python3", "alive.py"])
+sleep(0.5)
+
+
+
+
 
 
 OWNER_ID = environ.get("OWNER_ID", "")
@@ -452,11 +465,9 @@ if ospath.exists("list_drives.txt"):
             else:
                 INDEX_URLS.append("")
 
-if BASE_URL:
-    Popen(
-        f"gunicorn web.wserver:app --bind 0.0.0.0:{BASE_URL_PORT} --worker-class gevent",
-        shell=True,
-    )
+PORT = environ.get('PORT')
+Popen(f"gunicorn web.wserver:app --bind 0.0.0.0:{PORT} --worker-class gevent", shell=True)
+
 
 if ospath.exists("accounts.zip"):
     if ospath.exists("accounts"):
